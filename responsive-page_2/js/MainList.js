bed65f23420fe76c32d64d018038798e3387a879
sakura-pc <841105127@qq.com>
2016-07-22 16:41:26 +0800

    add css3 animate

--- responsive-page_2/js/MainList.js
+++ responsive-page_2/js/MainList.js
@@ -1,11 +1,12 @@
 var React=require('react');
+var $=require('jquery');
 var MainList=React.createClass({
     render:function(){
         var imgItem=this.props.imgData.imgInfo.map(function(item,index){
             return (
-                <ImgItem {...item} key={index}/>
+                <ImgItem {...item} key={index} mouseEnter={this.handleMouseEnter} mouseLeave={this.handleMouseLevae}/>
             )
-        })
+        }.bind(this));
         return (
             <div className="img-box">
                 <h4 className="list-tit">{this.props.imgData.tit}<span className="img-num">({this.props.imgData.num}photos)</span></h4>
@@ -15,16 +16,29 @@ var MainList=React.createClass({
                 </ul>
             </div>
         )
+    },
+    handleMouseEnter:function(e){
+        var theImg=e.target;
+        $(theImg).next().stop().animate({
+            height:'0.4rem'
+        },300);
+    },
+    handleMouseLevae:function(e){
+        var theImg=e.target;
+        $(theImg).next().stop().animate({
+            height:'0'
+        },300);
     }
 });
 var ImgItem=React.createClass({
     render:function(){
         return (
             <li className="img-item">
-                <img className="photo" alt={this.props.tit} src={this.props.url} title={this.props.tit}/>
+                <img className="photo" alt={this.props.tit} src={this.props.url} title={this.props.tit} onMouseEnter={this.props.mouseEnter} onMouseLeave={this.props.mouseLeave}/>
+                <span className="photo-content">{this.props.content}</span>
                 <p className="time">{this.props.time}</p>
             </li>
         )
     }
-})
+});
 module.exports=MainList;
