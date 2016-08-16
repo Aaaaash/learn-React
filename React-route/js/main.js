82d3c169095ad80bdbf7bc26a08b2a12eb6a2616
sakura-pc <841105127@qq.com>
2016-08-16 20:50:17 +0800

    router

--- React-route/js/main.js
+++ React-route/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router,Route,Link,hashHistory,IndexRoute,Redirect} from 'react-router';
+import {Router,Route,Link,hashHistory,IndexRoute,Redirect,IndexRedirect,IndexLink,browserHistory} from 'react-router';
 
 class App extends React.Component{
     render(){
@@ -18,7 +18,8 @@ class Repos extends React.Component{
     render(){
         return (
             <div>
-                <h3>科科</h3>
+                <h3>填写表单</h3>
+                {this.props.children}
             </div>
         )
     }
@@ -27,17 +28,57 @@ class About extends React.Component{
     render(){
         return(
             <div>
-                <Link to="/">点我返回首页</Link>
+                <IndexLink className="btn btn-default" to="/">点我返回首页</IndexLink>
             </div>
         )
     }
 }
-let routes=<Route path="/" component={App}>
-    <Route path="/repos" component={Repos} />
-    <Route path="/about" component={About} />
-</Route>;
+class Home extends React.Component{
+    render(){
+        return (
+            <div>
+                <Link className="btn btn-default" to="/about">点击加载About</Link>
+                <Link className="btn btn-default" to="/repos">点击加载Repos</Link>
+            </div>
+
+        )
+    }
+}
+class Form extends React.Component{
+    handleSubmit(event){
+        event.preventDefault();
+        const userName=event.target.elements[0].value;
+        const repo=event.target.elements[1].value;
+        const path=`/repos/${userName}/${repo}`
+        browserHistory.push(path)
+    }
+    render(){
+        return (
+            <div>
+                <h1>Welcome! React-router!</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="userName"/>
+                    <input type="text" placeholder="repo"/>
+                    <button type="submit">Go</button>
+                </form>
+            </div>
+        )
+    }
+
+}
 ReactDOM.render(
     (
-        <Router history={hashHistory} routes={routes}/>    
+        <Router history={hashHistory} >
+            <Route path="/" component={App}>
+                {/*<IndexRedirect to="/welcome"/>*/}
+
+                <IndexRoute component={Home}/>
+                <Redirect from="/repos" to="/repos/form"/>
+                <Route component={Repos}>
+                    <Route path="/repos/form" component={Form}/>
+                </Route>
+                <Route path="/about" component={About} />
+            </Route>
+        </Router>
     ),document.getElementById('app')
 );
